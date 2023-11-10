099dd67d2be7d65b61c32c2189202d6cb11317f5
Laurin Novak <laurin@pellervoinen.com>
2023-11-10 23:07:12 +0200

    assignment 9 - implement message broadcasting

--- assignment-09/ChatServer.ex
+++ assignment-09/ChatServer.ex
@@ -11,15 +11,36 @@
 # Allow client to assign a name to itself after its connected to the server.
 # When message is passed to other clients, show the name of the message originator.
 
+defmodule ChatServer.Clients do
+  use Agent
+
+  def start_link() do
+    Agent.start_link(fn -> [] end, name: __MODULE__)
+  end
+
+  def get_others(client) do
+    Agent.get(__MODULE__, fn clients -> List.delete(clients, client) end)
+  end
+
+  def add(client) do
+    Agent.update(__MODULE__, fn clients -> [client | clients] end)
+  end
+
+  def remove(client) do
+    Agent.update(__MODULE__, fn clients -> List.delete(clients, client) end)
+  end
+end
+
 defmodule ChatServer do
   require Logger
 
   def start() do
     children = [
       {Task.Supervisor, name: ChatServer.TaskSupervisor},
-      # {Task, fn -> accept(4040) end}
       Supervisor.child_spec({Task, fn -> accept(4040) end}, restart: :permanent)
     ]
+
+    ChatServer.Clients.start_link()
 
     opts = [strategy: :one_for_one, name: ChatServer.Supervisor]
 
@@ -39,16 +60,32 @@ defmodule ChatServer do
     {:ok, pid} = Task.Supervisor.start_child(ChatServer.TaskSupervisor, fn -> serve(client) end)
 
     :ok = :gen_tcp.controlling_process(client, pid)
+
+    ChatServer.Clients.add(client)
+
     Logger.info("Accepted client #{inspect(client)}")
     loop_accept(socket)
   end
 
-  defp serve(socket) do
+  defp serve(socket, name \\ "anon") do
     socket
     |> read_line()
-    |> write_line(socket)
+    |> String.trim()
+    |> String.split(":", parts: 2)
+    |> case do
+      ["name", name] -> serve(socket, name)
+      ["msg", msg] -> broadcast(socket, msg, name)
+      _ -> :ok
+    end
 
-    serve(socket)
+    serve(socket, name)
+  end
+
+  defp broadcast(socket, msg, name) do
+    Logger.info("Broadcasting #{msg} from #{name} for socket #{inspect(socket)}")
+
+    ChatServer.Clients.get_others(socket)
+    |> Enum.each(fn client -> write_line("#{name}: #{msg}\n", client) end)
   end
 
   defp read_line(socket) do
@@ -57,7 +94,6 @@ defmodule ChatServer do
   end
 
   defp write_line(line, socket) do
-    Task.Supervisor.children(ChatServer.TaskSupervisor)
     :gen_tcp.send(socket, line)
   end
 end
